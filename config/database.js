3970ace22df826d299d5831be2c2a6b32680a682
Kerlin Augustin <kerlin.augustin1@gmail.com>
2022-05-30 05:54:30 -0400

    first commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-    'dbName': 'demo'
+    'url' : 'mongodb+srv://kerlinaugustin:sneakyboi7@cluster0.iq03h.mongodb.net/cluster0?retryWrites=true&w=majority', 
+    'dbName': 'Cluster0'
 };
